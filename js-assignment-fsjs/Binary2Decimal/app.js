dd95c348de03e4b74778be6be823c7748f98b741
Yasir284 <lambawalay@gmail.com>
2022-09-20 14:55:46 +0530

    BinaryToDecimal project completed

--- js-assignment-fsjs/Binary2Decimal/app.js
+++ js-assignment-fsjs/Binary2Decimal/app.js
@@ -1,19 +1,44 @@
-function convert(decimal) {
-  let length = Math.floor(Math.log2(decimal));
-  let ans = 0;
-  while (length >= 0) {
-    const number = 2 ** length;
-    if (number <= decimal) {
-      ans += 10 ** length;
-      decimal -= number;
+let convertBtn = document.querySelector(".btn");
+let result = document.querySelector(".result");
+convertBtn.addEventListener("click", addDecimal);
+
+// Getting Decimal number
+function getDecimal(binary) {
+  binary = document.querySelector("#binary").value;
+  binary = binary.toString().split("");
+  let check = binary.every((num) => num == "0" || num == "1");
+  if (check) {
+    let length = binary.length;
+    let ans = 0;
+    let i = 0;
+    while (i < length) {
+      if (binary[i] == 1) {
+        let num = length - 1 - i;
+        ans += 2 ** num;
+      }
+      i++;
     }
-    length--;
+    return `Decimal Number : ${ans}`;
+  } else {
+    return `Number is not Binary!! Enter Valid Number`;
   }
-  return ans;
 }
-const start = Date.now();
 
-console.log(convert(165));
+// Adding decimal number
+function addDecimal(event) {
+  result.textContent = getDecimal(binary);
+  return event.preventDefault();
+}
 
-const end = Date.now();
-console.log(`Execution time: ${end - start} ms`);
+// ====== Decimal to binary ========
+// let length = Math.floor(Math.log2(decimal));
+// let ans = 0;
+// while (length > 0) {
+//   let num = 2 ** length;
+//   if (num <= decimal) {
+//     ans += 10 ** length;
+//     decimal -= num;
+//   }
+//   length--;
+// }
+// console.log(ans);
